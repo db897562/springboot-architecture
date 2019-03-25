730181de4d027a948c7c2f11cfdbe791eeed611b
dirk <754767663@qq.com>
2019-03-25 22:31:13 +0800

    init project

--- src/main/java/com/architecture/config/ExceptionHandlerAdvice.java
+++ src/main/java/com/architecture/config/ExceptionHandlerAdvice.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Result;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
